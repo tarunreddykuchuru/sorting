ff84d117b03c8f090191261235f6381c29de3ba1
tarun <tarun@DESKTOP-JGGPPQQ>
2021-02-11 19:16:18 +0530

    newsort

---
+++ src/sorting/Newsort.java
@@ -0,0 +1,19 @@
+package sorting;
+import java.util.*;
+public class Newsort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int l[] = 
+			{
+				89, -69, 76, 6, 57
+			};
+			Arrays.sort(l);
+			for (int k: l) 
+			{
+				System.out.println(k);
+			}
+
+	}
+
+}
